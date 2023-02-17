33c7a601b935d301fea2e34c51f3ee7a1c8b3f8d
Rahul Bhagat <rahulbhagat13slr@gmail.com>
2023-02-17 23:43:48 +0530

    First Commit

---
+++ calculator.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+class calculator {
+    calculator(){
+    Textfield t = new Textfield();
+    add(t);
+}
+    public static void main(String[] args) 
+        
+     {
+        new calculator();
+    }
+
+}
